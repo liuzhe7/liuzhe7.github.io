d10f1d50c81d00002b559998c991ec79684d0877
liuzhe7 <zhixizhixizhixi@gmail.com>
2024-11-23 17:08:13 +0800

    add image

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,72 @@
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div class="user-avatar">
+    <div class="image-container">
+      <img v-if="props.src" :src="props.src" :alt="props.alt" class="avatar-img" />
+    </div>
+    <div class="name-container">
+      <span v-if="props.name" class="avatar-name">{{ props.name }}</span>
+    </div>
+    <div class="description-container">
+      <p v-if="props.description" class="avatar-description">{{ props.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.user-avatar {
+  width: 200px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.image-container {
+  flex: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.avatar-img {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 1rem;
+  object-fit: cover;
+}
+
+.name-container {
+  flex: 0.5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.description-container {
+  flex: 5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,73 @@
+<script setup>
+import UserAvatar from '@/components/UserAvatar.vue'
+import CardLink from '@/components/CardLink.vue'
+
+const cards = [
+  {
+    id: 1,
+    img: '/src/assets/bilibili.png',
+    title: 'bilibili',
+    link: 'https://space.bilibili.com/319717159/',
+  },
+  {
+    id: 2,
+    img: '/src/assets/github.png',
+    title: 'github',
+    link: 'https://github.com/liuzhe7',
+  },
+  {
+    id: 3,
+    img: '/src/assets/redbook.png',
+    title: 'red book',
+    link: 'https://www.xiaohongshu.com/user/profile/5c21f0f7000000000700be21',
+  },
+  {
+    id: 4,
+    img: '/src/assets/instagram.png',
+    title: 'instagram',
+    link: 'https://www.instagram.com/liuzhe719/profilecard/?igsh=cDNmcmMwZnllcjhw',
+  },
+  {
+    id: 5,
+    img: '/src/assets/gmail.png',
+    title: 'gmail',
+    link: 'zhixizhixizhixi@gmail.com',
+  },
+  {
+    id: 6,
+    img: '/src/assets/wechat.png',
+    title: 'wechat',
+    link: 'weixin://contacts/profile/Jeodncjwodkjdirjemcm',
+  },
+]
+</script>
+
+<template>
+  <div class="home-view">
+    <div class="avatar-container">
+      <UserAvatar
+        name="Zhe Liu"
+        src="/src/assets/image.png"
+        alt="avatar"
+        description="Passionate Senior Frontend Developer with expertise in Vue.js, Nuxt, and TypeScript. Leading development teams at major tech companies for over 5 years. Specialized in building scalable web applications, optimizing performance, and implementing responsive designs. Regular speaker at tech conferences and contributor to open-source projects. Currently focused on microfront-end architecture and modern web development practices."
+        status="online"
+      />
+    </div>
+    <div class="card-link-container">
+      <CardLink :cards="cards" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  gap: 5rem;
+  padding: 1rem;
+  align-items: center;
+  background-color: bisque;
+}
+</style>
